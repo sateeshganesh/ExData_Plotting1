416d6959e1ed5430224eed3efc5be62524b19fe2
sateeshganesh <sateesh.ganesh@gmail.com>
2016-05-21 18:04:41 -0400

    Exploratory Data Analysis - Plotting

--- plot4.R
+++ plot4.R
@@ -20,9 +20,13 @@ hpcdata <- hpc %>% mutate(DateTime = as.POSIXct(strptime(paste(Date,Time),format
 
 # Step 4: Create Plot 4: Four plots in one page and store it in PNG file:
 png(filename = "./plot4.png", width = 480,height = 480, units = "px")
+par(mfcol=c(2,2))
+plot(hpcdata$DateTime, as.numeric(as.character(hpcdata$Global_active_power)), type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
 plot(hpcdata$DateTime, as.numeric(as.character(hpcdata$Sub_metering_1)), type = "n", ylab = "Energy sub metering", xlab = "")
 lines(hpcdata$DateTime, as.numeric(as.character(hpcdata$Sub_metering_1)), type = "l")
 lines(hpcdata$DateTime, as.numeric(as.character(hpcdata$Sub_metering_2)), type = "l", col = "red")
 lines(hpcdata$DateTime, as.numeric(as.character(hpcdata$Sub_metering_3)), type = "l", col = "blue")
 legend("topright", lty = c(1,1,1), col = c("black","red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+plot(hpcdata$DateTime, as.numeric(as.character(hpcdata$Voltage)), type = "l", ylab = "Voltage", xlab = "datetime")
+plot(hpcdata$DateTime, as.numeric(as.character(hpcdata$Global_reactive_power)), type = "l", ylab = "Global_reactive_power", xlab = "datetime")
 dev.off()
